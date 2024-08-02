41b07c7aab4e09244c8bb091151d1071b31ca51f
mel-muhina <mel.muhina@gmail.com>
2024-08-02 12:53:55 +0100

    Resolved small error with file directory

--- api/database/setup.js
+++ api/database/setup.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const fs = require("fs");
 
 const db = require("./connect");
-const sql = fs.readFileSync("./db/database.sql").toString();
+const sql = fs.readFileSync("./database/database.sql").toString();
 
 db.query(sql)
    .then(data => {
