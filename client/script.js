db948b9a14cfa3a13c7f2c502c858d96aada4385
“LiliiaVerbenko” <verbenkoliliia@gmail.com>
2024-08-04 02:19:28 +0100

    Added more frontend to keep up with Mel

--- client/script.js
+++ client/script.js
@@ -2,66 +2,50 @@ const diaryEntriesButton = document.querySelector("#loadDiaryEntries");
 const diaryEntries = document.querySelector("#diaryEntries");
 
 const getDiary = (e) => {
-    e.preventDefault() // Prevents the page from auto refreshing when submit is pressed
-    console.log("Pressing diary button")
-    getDiaryEntries()
+    e.preventDefault();
+    console.log("Loading diary entries...");
+    getDiaryEntries();
 }
 
-//Event listener that loads all the diary entries up
 diaryEntriesButton.addEventListener("click", getDiary);
 
-
-// Function to get all diary entries, currently not using the parameter id as I'm working on showing all entries first
-
-async function getDiaryEntries(id) {
+async function getDiaryEntries() {
     try {
-    const response = await fetch(`https://hackathondiary.onrender.com/diary`)
-    
-    if (response.ok) {
-        const data = await response.json() 
-
-        // I broke them down within the function just to test if they work individually 
-
-        // const authorName = data[0].author_name
-        // const category = data[0].category
-        // const title =  data[0].title
-        // const content = data[0].content
-        // const entry_date = data[0].entry_date
-
-        addEntry(data[0]) // Sending the entries data to the addEntry function 
-      
-    } else {
-        throw "Error: http status code = " + response.status
-    } 
- }
-catch (err) {
-    console.log(err)
+        const response = await fetch(`https://hackathondiary.onrender.com/diary`);
+        
+        if (response.ok) {
+            const data = await response.json();
+            diaryEntries.innerHTML = ''; // Clear previous entries
+            data.forEach(entry => addEntry(entry)); // Map over all entries
+        } else {
+            throw new Error("Error: " + response.status);
+        }
+    } catch (err) {
+        console.error(err);
     }
 }
 
-// Creating new elements every time the button is pressed to show all the entries under the button
-// Currently I have only used the first data in the database, it needs to be mapped to show all
-
 const addEntry = (data) => {
-    const { author_name, category, title, content, entry_date } = data
-    console.log("addEntry function", data)
+    const { author_name, category, title, content, entry_date } = data;
 
-    const h3 = document.createElement("h3")
-    const h4 = document.createElement("h4")
-    const date = document.createElement("h4")
-    const postContent = document.createElement("p")
+    const entryDiv = document.createElement("div");
 
-    h3.innerHTML = `Author: ${author_name}`
-    h4.innerHTML = `Category: ${category}`
-    date.innerHTML = `Date: ${entry_date}`
-    postContent.innerHTML = `Content: ${content}`
-  
-   
-    diaryEntries.appendChild(h3) 
-    diaryEntries.appendChild(h4) 
-    diaryEntries.appendChild(date) 
-    diaryEntries.appendChild(postContent) 
+    const authorElement = document.createElement("h3");
+    authorElement.textContent = `Author: ${author_name}`;
 
-     
+    const categoryElement = document.createElement("h4");
+    categoryElement.textContent = `Category: ${category}`;
+
+    const dateElement = document.createElement("h4");
+    dateElement.textContent = `Date: ${entry_date}`;
+
+    const contentElement = document.createElement("p");
+    contentElement.textContent = `Content: ${content}`;
+
+    entryDiv.appendChild(authorElement);
+    entryDiv.appendChild(categoryElement);
+    entryDiv.appendChild(dateElement);
+    entryDiv.appendChild(contentElement);
+
+    diaryEntries.appendChild(entryDiv);
 }
-
